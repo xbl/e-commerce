7b36c9a6a3e80abb4c35329064ea4450a6559ae2
xbl <x_bl@sina.com>
2020-08-17 15:47:03 +0800

    project init

---
+++ src/main/java/com/xbl/ecommerce/modules/IndexController.java
@@ -0,0 +1,15 @@
+package com.xbl.ecommerce.modules;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/")
+public class IndexController {
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public String index() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/xbl/ecommerce/modules/IndexControllerTest.java
@@ -0,0 +1,34 @@
+package com.xbl.ecommerce.modules;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+public class IndexControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup(WebApplicationContext wac) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void index() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().string(containsString("hello")));
+    }
+}
