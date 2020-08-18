af14634e0f7fea960371fda5ad59b93c83621064
xbl <x_bl@sina.com>
2020-08-18 15:22:55 +0800

    feat(Index):add togglz

--- src/main/java/com/xbl/ecommerce/index/IndexController.java
+++ src/main/java/com/xbl/ecommerce/index/IndexController.java
@@ -4,20 +4,27 @@ import com.xbl.ecommerce.index.entity.Index;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.util.NamedFeature;
 
 @RestController
 @RequestMapping(value="/")
 public class IndexController {
 
     private IndexService indexService;
+    private FeatureManager manager;
 
-    public IndexController(IndexService indexService) {
+    public IndexController(IndexService indexService, FeatureManager manager) {
         this.indexService = indexService;
+        this.manager = manager;
     }
 
     @RequestMapping(value="/", method= RequestMethod.GET)
     public Index index() {
         Index index = this.indexService.get(2);
+        if (manager.isActive(new NamedFeature("FOO"))) {
+            index.setName(index.getName() + " is active");
+        }
         return index;
     }
 }
--- src/test/java/com/xbl/ecommerce/index/IndexControllerTest.java
+++ src/test/java/com/xbl/ecommerce/index/IndexControllerTest.java
@@ -13,7 +13,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 @SpringBootTest
 public class IndexControllerTest {
 
