051ff3f1a42b12ffee3b094d42343e83b137a81b
xbl <x_bl@sina.com>
2020-08-18 09:54:29 +0800

    feat(Index):add lombok dependencies

---
+++ src/main/java/com/xbl/ecommerce/index/entity/Index.java
@@ -0,0 +1,15 @@
+package com.xbl.ecommerce.index.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+public class Index {
+    private Integer id;
+    private String name;
+}
